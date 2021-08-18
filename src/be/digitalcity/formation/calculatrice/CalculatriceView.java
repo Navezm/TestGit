f6e41fedeb78645ad22907f053927180bcff8726
laetcrul <laetcrul@gmail.com>
2021-08-18 15:39:11 +0200

    added enterNumbers method to add answers to list

--- src/be/digitalcity/formation/calculatrice/CalculatriceView.java
+++ src/be/digitalcity/formation/calculatrice/CalculatriceView.java
@@ -1,11 +1,14 @@
 package be.digitalcity.formation.calculatrice;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class CalculatriceView {
 
     static boolean stillRunning;
+    static List<Double> numbers = new ArrayList();
 
     public static void open() {
         System.out.println("-------CALCULATRICE-------");
@@ -24,16 +27,16 @@ public class CalculatriceView {
         int choix = choisirChiffre(4);
 
         switch (choix) {
-            case 1:{
+            case 1: {
 
             }
-            case 2:{
+            case 2: {
 
             }
-            case 3:{
+            case 3: {
 
             }
-            case 4:{
+            case 4: {
 
             }
         }
@@ -46,7 +49,7 @@ public class CalculatriceView {
             try {
                 Scanner scan = new Scanner(System.in);
                 choix = scan.nextInt();
-                if(choix >= 1 && choix <= nombreOptions){
+                if (choix >= 1 && choix <= nombreOptions) {
                     caractereValide = true;
                 } else {
                     System.out.println("Veuillez entrer un chiffre entre 1 et " + nombreOptions);
@@ -58,5 +61,50 @@ public class CalculatriceView {
             }
         }
         return choix;
+    }
+
+    private static void enterNumbers() {
+        System.out.println("Entrez le premier nombre");
+        while(tryNewAnswer()){
+            System.out.println("Entrez le premier nombre");
+            tryNewAnswer();
+        }
+
+        System.out.println("Entrez le nombre suivant");
+        while(tryNewAnswer()){
+            System.out.println("Entrez le nombre suivant");
+            tryNewAnswer();
+        }
+
+        System.out.println("Souhaitez-vous entrer un autre nombre?\n" +
+                "1) Oui\n" +
+                "2) Non");
+        int answer = choisirChiffre(2);
+
+        while (answer == 1) {
+            System.out.println("Entrez le nombre suivant");
+            while(tryNewAnswer()){
+                System.out.println("Entrez le nombre suivant");
+                tryNewAnswer();
+            }
+
+            System.out.println("Souhaitez-vous entrer un autre nombre?\n" +
+                    "1) Oui\n" +
+                    "2) Non");
+            answer = choisirChiffre(2);
+        }
+    }
+
+    private static boolean tryNewAnswer(){
+        Scanner scan = new Scanner(System.in);
+        try{
+            double answer = scan.nextInt();
+            numbers.add(answer);
+            return false;
+
+        } catch (InputMismatchException e){
+            System.err.println("Caractère invalide");
+            return true;
+        }
     }
 }
