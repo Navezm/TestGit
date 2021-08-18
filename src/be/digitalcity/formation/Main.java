d2beeeca5891cf0c8045c16a266b17217c7a5acf
DogmaticCat <sin.midani@gmail.com>
2021-08-18 15:40:30 +0200

    Pendu 0.3

--- src/be/digitalcity/formation/Main.java
+++ src/be/digitalcity/formation/Main.java
@@ -7,7 +7,7 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-
+        ;
 
     }
     public List dictionnaire(){
--- src/be/digitalcity/formation/Pendu2.java
+++ src/be/digitalcity/formation/Pendu2.java
@@ -1,48 +1,55 @@
 package be.digitalcity.formation;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Pendu2 {
+
+    // Liste de mots disponibles
+    static String[] LISTE = {"Lapin", "Tortue", "Chaise", "Voiture", ""};
 
     // Compteur d'erreurs, il commence à 0
     static int compteurErreurs =0;
 
-
-    public static void main(String[] args) {
-
-        // Liste de mots disponibles
-        String[] LISTE = {};
-        //
-
-    }
+    // Mot à deviner
+    static String motChoisi = motAleatoire(LISTE);
 
     // Selectionne un mot dans liste
-    public String motAleatoire (String[] liste){
+    static String motAleatoire (String[] liste){
         String motChoisi = "";
-        // ....
-        return motChoisi;
+        int index = new Random().nextInt(liste.length);
+        return liste[index];
     }
 
-    // Le joueur rentre une lettre
-    public void userInput () {
-        if (isCorrect){
 
-        }
-        else () {
-            compteurErreurs++;
-        }
-
-    }
 
     public void jeu() {
         while (compteurErreurs < 6) {
             //...
+            userInputValidation();
+            //...
         }
+        System.out.println("Vous avez perdu ! Le mot était " + motChoisi);
     }
 
     // Vérifie si la lettre est présente
-    isCorrect () {
+    boolean isCorrect (char x) {
+        //...
         return true;
     }
 
+    // Le joueur rentre une lettre
+    public void userInputValidation () {
+        Scanner scanner = new Scanner(System.in);
+        char e = scanner.nextLine().charAt(0);
 
+        if (isCorrect(e)){
+            //...
+
+        }
+        else {
+            compteurErreurs++;
+        }
+    }
 
 }
