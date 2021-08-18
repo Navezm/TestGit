9ea378453943f811b373fbe77e671db6f8e5e698
DogmaticCat <sin.midani@gmail.com>
2021-08-18 15:17:45 +0200

    Pendu 0.2

--- src/be/digitalcity/formation/Pendu2.java
+++ src/be/digitalcity/formation/Pendu2.java
@@ -2,14 +2,16 @@ package be.digitalcity.formation;
 
 public class Pendu2 {
 
+    // Compteur d'erreurs, il commence à 0
+    static int compteurErreurs =0;
+
+
     public static void main(String[] args) {
 
         // Liste de mots disponibles
         String[] LISTE = {};
-
         //
 
-        System.out.println("Hehe");
     }
 
     // Selectionne un mot dans liste
@@ -18,5 +20,29 @@ public class Pendu2 {
         // ....
         return motChoisi;
     }
+
+    // Le joueur rentre une lettre
+    public void userInput () {
+        if (isCorrect){
+
+        }
+        else () {
+            compteurErreurs++;
+        }
+
+    }
+
+    public void jeu() {
+        while (compteurErreurs < 6) {
+            //...
+        }
+    }
+
+    // Vérifie si la lettre est présente
+    isCorrect () {
+        return true;
+    }
+
+
 
 }
