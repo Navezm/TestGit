f099c803e054103a7599e3b7f9894c6e112c3352
Navezm <navez.martin@gmail.com>
2021-08-18 14:18:32 +0200

    Refactor

--- src/be/digitalcity/formation/Main.java
+++ src/be/digitalcity/formation/Main.java
@@ -3,11 +3,6 @@ package be.digitalcity.formation;
 public class Main {
 
     public static void main(String[] args) {
-
-
-
-
-	// write your code here
         System.out.println("On va planter ton programme");
         System.out.println("Et on y va");
         System.out.println("dddd");
