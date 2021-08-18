e19b786060554d69a1ad39d1649dbeb421dbf73a
Navezm <navez.martin@gmail.com>
2021-08-18 12:36:13 +0200

    Add sout

--- src/be/digitalcity/formation/Main.java
+++ src/be/digitalcity/formation/Main.java
@@ -6,5 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("On va planter ton programme");
         System.out.println("dddd");
+
+        System.out.println("Koukou cé mwa");
     }
 }
