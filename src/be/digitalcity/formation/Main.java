0c88567861f7514299bd38b6fdba2f082a6d5eb0
Ezaabouji Zineb <zineb.ezaabouji@hotmail.com>
2021-08-18 15:17:42 +0200

    Pendu seconde implémentation Z

--- src/be/digitalcity/formation/Main.java
+++ src/be/digitalcity/formation/Main.java
@@ -1,6 +1,8 @@
 package be.digitalcity.formation;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
 
@@ -9,16 +11,25 @@ public class Main {
 
     }
     public List dictionnaire(){
-        return null;
+        List<String> dictonnaire = new ArrayList<>();
+        dictonnaire.add("bonjour");
+        dictonnaire.add("moto");
+        dictonnaire.add("velo");
+        dictonnaire.add("voiture");
+        return dictonnaire;
     }
     public int genererMot(){
-        return 0;
+        return new Random().nextInt(dictionnaire().size());
     }
-    public void parcourirMot(List motATrouver, List lettreTrouver){
-
+    public void parcourirMot(String motATrouver, char caractereEntrer,List lettreTrouver, int indice){
+        //((String)dictionnaire().get(i)).length()
+        for (int i = 0; i < motATrouver; i++) {
+            if(motATrouver.charAt(i)==caractereEntrer){
+                placerLettre(motATrouver.charAt(i), i);
+            }
+        }
     }
     public void placerLettre(char lettre, int indice){
-
     }
 
     public void afficherMot(List mot) {
