2e051abd1581b9da645becffc8bf83eedb9a0f04
Navezm <navez.martin@gmail.com>
2021-08-18 14:52:22 +0200

    Add Reader

---
+++ src/be/digitalcity/formation/devant/Reader.java
@@ -0,0 +1,16 @@
+package be.digitalcity.formation.devant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reader {
+
+    public static List<String> lireFichier(String path) {
+        List<String> personneList = new ArrayList<>();
+
+
+
+        return personneList;
+    }
+
+}
