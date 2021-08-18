8a28fa81ec4594433aa8b2add9cc8fab33e697c9
Oldberamot <daune.arnaud@gmail.com>
2021-08-18 15:39:53 +0200

    ajout de la fonctionnalité Quitter et castNumbers

--- src/be/digitalcity/formation/calculatrice/CalculatriceView.java
+++ src/be/digitalcity/formation/calculatrice/CalculatriceView.java
@@ -1,11 +1,13 @@
 package be.digitalcity.formation.calculatrice;
 
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class CalculatriceView {
 
     static boolean stillRunning;
+    static List<Double> numbers;
 
     public static void open() {
         System.out.println("-------CALCULATRICE-------");
@@ -20,21 +22,31 @@ public class CalculatriceView {
         System.out.println("1) Addition\n" +
                 "2) Soustraction\n" +
                 "3) Multiplication\n" +
-                "4) Division");
+                "4) Division\n" +
+                "5) Quitter\n"
+        );
         int choix = choisirChiffre(4);
 
         switch (choix) {
             case 1:{
 
+                Calculatrice.addition(castNumbers());
             }
             case 2:{
 
+                Calculatrice.substraction(castNumbers());
             }
             case 3:{
 
+                Calculatrice.multiplication(castNumbers());
             }
             case 4:{
 
+                Calculatrice.division(castNumbers());
+            }
+            case 5: {
+                System.out.println("Aurevoir!");
+                stillRunning = false;
             }
         }
     }
@@ -58,5 +70,13 @@ public class CalculatriceView {
             }
         }
         return choix;
+    }
+
+    public static double[] castNumbers() {
+        double [] args = new double[numbers.size()];
+        for (int i = 0 ; i < args.length ; i++) {
+            args[i]= numbers.get(i);
+        }
+        return args;
     }
 }
