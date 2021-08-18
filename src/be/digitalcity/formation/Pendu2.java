6169c98d21c282009c35e1c553eee40d52a989ba
DogmaticCat <sin.midani@gmail.com>
2021-08-18 15:06:50 +0200

    Pendu Sinann

---
+++ src/be/digitalcity/formation/Pendu2.java
@@ -0,0 +1,22 @@
+package be.digitalcity.formation;
+
+public class Pendu2 {
+
+    public static void main(String[] args) {
+
+        // Liste de mots disponibles
+        String[] LISTE = {};
+
+        //
+
+        System.out.println("Hehe");
+    }
+
+    // Selectionne un mot dans liste
+    public String motAleatoire (String[] liste){
+        String motChoisi = "";
+        // ....
+        return motChoisi;
+    }
+
+}
