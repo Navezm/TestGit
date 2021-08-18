4ffef512e9cd371cd498e10aa78db706f057c662
drallas123 <s.allard789@gmail.com>
2021-08-18 12:36:18 +0200

    test

--- src/be/digitalcity/formation/Main.java
+++ src/be/digitalcity/formation/Main.java
@@ -3,6 +3,10 @@ package be.digitalcity.formation;
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("Coucou les amis comment ca va?");
+
 	// write your code here
+
     }
 }
