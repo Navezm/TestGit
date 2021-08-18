db254b5176cb78cd9bb52d26ba4b7798d1bbcaf4
laetcrul <laetcrul@gmail.com>
2021-08-18 14:55:27 +0200

    methodes implementees : addition et soustraction

--- src/be/digitalcity/formation/Main.java
+++ src/be/digitalcity/formation/Main.java
@@ -1,12 +1,10 @@
 package be.digitalcity.formation;
 
+import be.digitalcity.formation.calculatrice.Calculatrice;
+
 public class Main {
 
     public static void main(String[] args) {
-
-
-
-
 	// write your code here
         System.out.println("On va planter ton programme");
         System.out.println("Et on y va");
@@ -20,4 +18,5 @@ public class Main {
         System.out.println("omg j'ai tellement faim je vais mourir");
         System.out.println("Coucou les amis comment ca va?");
     }
+
 }
--- src/be/digitalcity/formation/calculatrice/Calculatrice.java
+++ src/be/digitalcity/formation/calculatrice/Calculatrice.java
@@ -1,4 +1,30 @@
 package be.digitalcity.formation.calculatrice;
 
+import java.util.Arrays;
+
 public class Calculatrice {
+
+    public static double addition(double... numbers){
+        if (numbers.length > 0) {
+            return Arrays.stream(numbers).sum();
+        }
+        else System.err.println("No numbers entered");
+        return 0;
+    }
+
+    public static double substraction(double... numbers) {
+        double total = 0;
+        if (numbers.length > 1) {
+            total = numbers[0];
+            for (int i = 0; i < numbers.length; i++) {
+                if (i == 0) {
+                    continue;
+                } else{
+                    total = total - numbers[i];
+                }
+            }
+        } else System.out.println("At least two numbers required");
+        return total;
+    }
 }
+
