07da686b8c55d4952e23dffe5cc341d6330babe2
Ezaabouji Zineb <zineb.ezaabouji@hotmail.com>
2021-08-18 15:03:24 +0200

    Pendu première implémentation Z

--- src/be/digitalcity/formation/Main.java
+++ src/be/digitalcity/formation/Main.java
@@ -1,19 +1,28 @@
 package be.digitalcity.formation;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("On va planter ton programme");
-        System.out.println("Et on y va");
-        System.out.println("dddd");
-        System.out.println("Koukou cé mwa");
-        System.out.println("LAISSEZ MA LIGNE TRANQUILLE LAAAA");
-        System.out.println("Ici c'est Greg");
-        System.out.println("Elles arrivent quand ces pizzas ??");
-        System.out.println("chaaaaooooos");
-        System.out.println("bientot j'espère");
-        System.out.println("omg j'ai tellement faim je vais mourir");
-        System.out.println("Coucou les amis comment ca va?");
+
+
+    }
+    public List dictionnaire(){
+        return null;
+    }
+    public int genererMot(){
+        return 0;
+    }
+    public void parcourirMot(List motATrouver, List lettreTrouver){
+
+    }
+    public void placerLettre(char lettre, int indice){
+
+    }
+
+    public void afficherMot(List mot) {
+
         System.out.println("Hehe");
     }
 }
