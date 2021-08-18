28747b999008edbca6b56c1ae2f5a7fe780d3565
drallas123 <s.allard789@gmail.com>
2021-08-18 14:52:01 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/vcs.xml
    #	src/be/digitalcity/formation/Main.java

---
+++ src/be/digitalcity/formation/devant/Personne.java
@@ -0,0 +1,21 @@
+package be.digitalcity.formation.devant;
+
+import lombok.Data;
+
+@Data
+public class Personne {
+
+    public long id;
+    public String first_name;
+    public String last_name;
+    public String email;
+    public String gender;
+
+    public Personne(long id, String first_name, String last_name, String email, String gender) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.gender = gender;
+    }
+}
