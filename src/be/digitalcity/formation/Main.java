dedcb12458f777273601a34ff27ea72a2d996bbe
Navezm <navez.martin@gmail.com>
2021-08-18 14:41:12 +0200

    Add file csv

--- src/be/digitalcity/formation/Main.java
+++ src/be/digitalcity/formation/Main.java
@@ -4,20 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-
-
-	// write your code here
-        System.out.println("On va planter ton programme");
-        System.out.println("Et on y va");
-        System.out.println("dddd");
-        System.out.println("Koukou cé mwa");
-        System.out.println("LAISSEZ MA LIGNE TRANQUILLE LAAAA");
-        System.out.println("Ici c'est Greg");
-        System.out.println("Elles arrivent quand ces pizzas ??");
-        System.out.println("chaaaaooooos");
-        System.out.println("bientot j'espère");
-        System.out.println("omg j'ai tellement faim je vais mourir");
-        System.out.println("Coucou les amis comment ca va?");
     }
 }
